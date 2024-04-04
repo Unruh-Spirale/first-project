f671d953aa3c52af18eac5998e97c9271cac012c
Unruh-Spirale <protjan@wp.pl>
2024-04-04 07:19:03 +0200

    delete min-width from main.css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    
+    <div>
+      <button class="btn">-</button>
+      <span class="counter">0</span>
+      <button class="btn">+</button>
+    </div>
+
   </div>
 </template>
