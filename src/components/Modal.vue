cfa96a0f44d123020dee9769495af1390abf49e3
Unruh-Spirale <protjan@wp.pl>
2024-04-11 23:07:51 +0200

    Child Components

---
+++ src/components/Modal.vue
@@ -0,0 +1,26 @@
+<template>
+    <teleport to=".modals-container">
+        <div class="modal">
+            <h1>This is modal</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab suscipit nisi dolorum architecto,
+                quos, voluptate nemo itaque rerum beatae quo hic. Nobis alias eligendi quidem quasi adipisci
+                expedita libero explicabo.</p>
+            <button>Hide modal</button>
+        </div>
+    </teleport>
+</template>
+<script setup>
+</script>
+<style>
+.modal{
+    background: beige;
+    color: black;
+    padding: 10px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+}
+</style>
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,18 +2,7 @@
     <div class="modals" >
         <h1>Modals</h1>
         <button @click="showModal = true">Show modal</button>
-        <teleport to=".modals-container">
-            <div 
-            v-if="showModal"
-            class="modal"
-        >
-            <h1>This is modal</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab suscipit nisi dolorum architecto, 
-                quos, voluptate nemo itaque rerum beatae quo hic. Nobis alias eligendi quidem quasi adipisci 
-                expedita libero explicabo.</p>
-            <button @click="showModal = false">Hide modal</button>
-        </div>
-        </teleport>
+        <Modal v-if="showModal"></Modal>
 
     </div>
 </template>
@@ -22,7 +11,8 @@
 /*
     imports
 */
-import { ref } from 'vue';
+import { ref } from 'vue'
+import Modal from '@/components/Modal.vue'
 
 /*
     modals
@@ -32,16 +22,3 @@ const showModal = ref(false)
 
 </script>
 
-<style>
-.modal{
-    background: beige;
-    color: black;
-    padding: 10px;
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 1;
-}
-</style>
