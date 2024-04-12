5feea8a80bc5f43022b1b868db1062acb3071911
Unruh-Spirale <protjan@wp.pl>
2024-04-12 11:38:46 +0200

    Dynamic Components

---
+++ src/components/ModalDark.vue
@@ -0,0 +1,53 @@
+<template>
+    <teleport to=".modals-container">
+        <div 
+            v-if="modelValue"
+            class="modal-dark"
+        >
+            <h1>{{ title }}</h1>
+            <slot />
+            <button @click="handleButtonClick">Hide modal</button>
+        </div>
+    </teleport>
+</template>
+
+<script setup>
+/*
+    props
+*/
+const props = defineProps({
+    modelValue: {
+        type: Boolean,
+        default: false
+    },
+    title: {
+        type: String,
+        default: 'no title specifed'
+    }
+})
+/*
+    emits
+*/
+const emit = defineEmits(['update:modelValue'])
+/*
+    handle button click
+*/
+const handleButtonClick = () => {
+    console.log('handleButtonClick')
+    emit('update:modelValue', false)
+}
+</script>
+
+<style>
+.modal-dark{
+    background: #333;
+    color: white;
+    padding: 10px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+}
+</style>
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -1,16 +1,23 @@
 <template>
     <div class="modals" >
         <h1>Modals</h1>
+        <div>
+            <label>
+                Show dark modals?
+                <input v-model="showDarkModals" type="checkbox" />
+            </label>
+        </div>
         <button @click="showModal = true">Show modal</button>
-        <Modal 
+        <component
             v-model="showModal"
+            :is="showDarkModals ? ModalDark : Modal"
             title="My modal title (via prop)"
         >
             
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab suscipit nisi dolorum architecto,
                 quos, voluptate nemo itaque rerum beatae quo hic. Nobis alias eligendi quidem quasi adipisci
                 expedita libero explicabo.</p>
-        </Modal>
+        </component>
 
     </div>
 </template>
@@ -21,11 +28,12 @@
 */
 import { ref } from 'vue'
 import Modal from '@/components/Modal.vue'
+import ModalDark from '@/components/ModalDark.vue'
 
 /*
     modals
 */
-
+const showDarkModals= ref(false)
 const showModal = ref(false)
 
 </script>
